b8469f99e1f74ebeb3bb2f30cc7122ad0ef05db3
KuroM0M0 <m0m0_lp@web.de>
2025-10-14 01:42:38 +0200

    cards update

--- static/js/cards.js
+++ static/js/cards.js
@@ -9,22 +9,17 @@ socket.on("cards_update", function(data) {
             card.innerHTML = `
                 <div class="card">
                     <header class="card-header">
-                        <p class="zentriert card-header-title"><span title="SlotID">${username}</span> | ${username}</p>
+                        <p class="zentriert card-header-title"><span title="Username">${username}</span></p>
                     </header>
                     <div class="card-content">
                         <div class="content">
-                            <span title="Raum">🚪</span> ${username} <br>
-                            <span title="Regal">📥</span> ${username} <br>
-                            <span title="Fach">📦</span> ${username} <br>
-                            <span title="Typ">${username}</span>
+                            <input class="input" type="text" placeholder="Textfeld">
                         </div>
                     </div>
                 <footer class="card-footer">
-                
                         <p class="card-footer-item">
-                            <span title="Menge">${username}</span>
-                            <button class="button is-success is-small abstandMedium" onclick="addCount(${username})"><i class="fas fa-plus"></i></button>
-                            <button class="button is-danger is-small abstandSmall" onclick="decreaseCount(${username}, ${username})"><i class="fas fa-minus"></i></button>    
+                            <button class="button is-success is-small abstandMedium"><i class="fas fa-plus"></i></button>
+                            <button class="button is-danger is-small abstandSmall"><i class="fas fa-minus"></i></button>    
                         </p>
                     </footer>
                 </div>
