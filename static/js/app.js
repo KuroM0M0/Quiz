49e4b2edfcc1443600c43cd34baff5e7bacfc1ba
KuroM0M0 <m0m0_lp@web.de>
2025-10-20 15:01:31 +0200

    checkUsername - need fix

--- static/js/app.js
+++ static/js/app.js
@@ -17,3 +17,19 @@ function loadPage(page) {
         })
         .catch(error => console.error("Fehler beim Laden der Seite: ", error))
 }
+
+function CheckNameExists(name) {
+    fetch("/check_name", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name})
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data == true) {
+            ShowErrorAlert("Fehler", `Username ${data.name} existiert bereits. Bitte wähle einen anderen.`);
+        } else {
+            window.location.href = "/lobby";
+        }
+    })
+}
